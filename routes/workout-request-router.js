7d4a833cf41b5bac0b6bd3dc3d390d532c80e83b
Pras-hant-karki <gymratprashant911@gmail.com>
2025-02-27 00:05:47 +0545

    added extra router in workout request

--- routes/workout-request-router.js
+++ routes/workout-request-router.js
@@ -26,5 +26,12 @@ router.get("/pending", verifyUser, getPendingRequests);
 // ✅ Get only completed (responded) requests (User/Admin)
 router.get("/completed", verifyUser, getCompletedRequests);
 
+// User or Admin can get a specific request (Admin can see all, User can see only their own)
+router.get("/:id", verifyUser, getRequestById);
+router.put("/:id", verifyUser, updateRequest);
+router.delete("/:id", verifyUser, deleteRequest);
+
+// Admin can respond to a workout request
+router.put("/:id/respond", verifyUser,verifyAdmin, respondToRequest);
 
 module.exports = router;
