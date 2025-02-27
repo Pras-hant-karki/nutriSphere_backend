a6d2b6e725a0e0f97854eab146e0701c29f89112
Pras-hant-karki <gymratprashant911@gmail.com>
2025-02-27 17:52:59 +0545

    Update database connection

--- database/database.js
+++ database/database.js
@@ -1,13 +1,12 @@
 const { Sequelize } = require("sequelize");
 
+const dbName = process.env.NODE_ENV === "test" ? "test_prashant" : "prashant";
 
-const sequelize= new Sequelize('Nutrisphere','postgres','CarbonKurt33',{
-    host:'localhost',
-    dialect:'postgres',
-    port: 5432,
-    logging: false,
-})
+const sequelize = new Sequelize(dbName, "postgres", "admin123", {
+  host: "localhost",
+  dialect: "postgres",
+  port: 5432,
+  logging: false,
+});
 
-
-
-module.exports=sequelize;
+module.exports = sequelize;
